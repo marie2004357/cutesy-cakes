731561f1b193db9a4c5683a6f05f633724eb324b
marie2004357 <32556959+marie2004357@users.noreply.github.com>
2017-11-16 23:02:58 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//This controls the response box that says "under construction" displayed on the Contact us page.
+
 function myFunction() {
     alert("I'm so sorry this feature is under construction.  Please contact us by email or phone");
 }
