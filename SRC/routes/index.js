9af9901560bf3c5e5112391d77001e81f4f5ea7a
Marie <marie2004357@gmail.com>
2018-03-27 00:14:00 -0400

    added a form

--- SRC/routes/index.js
+++ SRC/routes/index.js
@@ -2,60 +2,51 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 
+
+// Totally fake data
 const FILES = [
-  {id: '1', name: 'Sleeping Baby', Description: 'Vanilla cake with decorative icing'},
-  {id: '2', name: 'Ladybug Character', Description: 'Red velvet cake with decorative icing'},
-  {id: '3', name: 'Elmo', Description: 'Yellow cake with decorative icing'},
-  {id: '4', name: 'Minnie Mouse', Description: 'Dark Chocolate cake with decorative icing'},
-  {id: '5', name: 'Monkey', Description: 'White cake with decorative icing'},
-  {id: '6', name: 'Peacock', Description: 'Multi-colored cake with decorative icing'},
-  {id: '7', name: 'Poop Emoji', Description: 'Chocolate cake with decorative icing'},
-  {id: '8', name: 'Chanel Purse', Description: 'White Chocolate cake with decorative icing'},
-  {id: '9', name: 'Panda', Description: 'Marble cake with decorative icing'},
-  {id: '10', name: 'Santa', Description: 'White mint chocolate cake with decorative icing'},
+  {id: 'a', title: 'cutecat1.jpg', description: 'A cute cat'},
+  {id: 'b', title: 'uglycat1.jpg', description: 'Just kidding, all cats are cute'},
+  {id: 'c', title: 'total_recall_poster.jpg', description: 'Quaid, start the reactor...'},
+  {id: 'd', title: 'louisville_coffee.txt', description: 'Coffee shop ratings'},
 ];
+//const FILES = [
+  //{id: '1', name: 'Sleeping Baby', Description: 'Vanilla cake with decorative icing'},
+  //{id: '2', name: 'Ladybug Character', Description: 'Red velvet cake with decorative icing'},
+  //{id: '3', name: 'Elmo', Description: 'Yellow cake with decorative icing'},
+  //{id: '4', name: 'Minnie Mouse', Description: 'Dark Chocolate cake with decorative icing'},
+  //{id: '5', name: 'Monkey', Description: 'White cake with decorative icing'},
+  //{id: '6', name: 'Peacock', Description: 'Multi-colored cake with decorative icing'},
+  //{id: '7', name: 'Poop Emoji', Description: 'Chocolate cake with decorative icing'},
+  //{id: '8', name: 'Chanel Purse', Description: 'White Chocolate cake with decorative icing'},
+ // {id: '9', name: 'Panda', Description: 'Marble cake with decorative icing'},
+  //{id: '10', name: 'Santa', Description: 'White mint chocolate cake with decorative icing'},
+//];
 
 
 router.use('/doc', function(req, res, next) {
   res.end(`Documentation http://expressjs.com/`);
 });
 
+/**
+ * Get a list of all files in the DB
+ */
 router.get('/file', function(req, res, next) {
-  mongoose.model('File').find({}, function(err, files) {
+  const fileModel = mongoose.model('File');
+
+  fileModel.find({}, function(err, files) {
     if (err) {
       console.log(err);
-      res.status(500).json(err);
+      return res.status(500).json(err);
     }
-
-  res.json(FILES);
-});
-});
-
-
-router.post('/file', function(req, res, next) {
-  const newId = '' + FILES.length;
-  const data = req.body;
-  data.id = newId;
-
-  FILES.push(data);
-  res.status(201).json(data);
-});
-router.put('/file/:fileId', function(req, res, next) {
-  const {fileId} = req.params;
-  const file = FILES.find(entry => entry.id === fileId);
-  if (!file) {
-    return res.status(404).end(`Could not find file '${fileId}'`);
-  }
-
-  file.id = req.body.id;
-  file.description = req.body.description;
-  res.json(file);
+  
+    res.json(files);
+  });
 });
 
-router.delete('/file/:fileId', function(req, res, next) {
-  res.end(`Deleting file '${req.params.fileId}'`);
-});
-
+/**
+ * Get a single file by passing its id as a URL param
+ */
 router.get('/file/:fileId', function(req, res, next) {
   const {fileId} = req.params;
   // same as 'const fileId = req.params.fileId'
@@ -68,5 +59,63 @@ router.get('/file/:fileId', function(req, res, next) {
   res.json(file);
 });
 
-module.exports = router;
+/**
+ * Create a new file
+ */
+router.post('/file', function(req, res, next) {
+  const File = mongoose.model('File');
+  const fileData = {
+    title: req.body.title,
+    description: req.body.description,
+  };
 
+  File.create(fileData, function(err, newFile) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+
+    res.json(newFile);
+  });
+});
+
+/**
+ * Update an existing file
+ */
+router.put('/file/:fileId', function(req, res, next) {
+  const File = mongoose.model('File');
+  const fileId = req.params.fileId;
+
+ // File.findById(fileId, function(err, file) {
+   // if (err) {
+     // console.error(err);
+     // return res.status(500).json(err);
+  //  }
+   // if (!file) {
+    //  return res.status(404).json({message: "File not found"});
+   // }
+
+    //file.title = req.body.title;
+    //file.description = req.body.description;
+
+    //file.save(function(err, savedFile) {
+      //if (err) {
+        //console.error(err);
+        //return res.status(500).json(err);
+     // }
+      res.json(savedFile);
+    })
+
+  //})
+
+//});
+
+/**
+ * Delete a file
+ */
+router.delete('/file/:fileId', function(req, res, next) {
+  res.end(`Deleting file '${req.params.fileId}'`);
+});
+
+
+module.exports = router;
