f1155de9e5cc3bfa2bb28762c8be4dc74f66c332
marie2004357 <32556959+marie2004357@users.noreply.github.com>
2018-03-30 03:00:56 -0400

    Update index.js

--- SRC/config/index.js
+++ SRC/config/index.js
@@ -1,4 +1,4 @@
-// src/config/index.js
+// src/config/index.js (note the host name is 'localhost'--this is typed in the url line of your browser)
   module.exports = {
     appName: 'Cutesy Cakes',
     port: 3030,
